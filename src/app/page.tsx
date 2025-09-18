0d2fa1f113d0ddb938e05b37baebfa4524cf4503
SAHIL YADAV <121011155+sahil-kumar-yadav@users.noreply.github.com>
2025-09-18 11:36:32 +0000

    wrapped root layout with convexclerprovider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 
+import ConvexClerkProvider from "@/components/Providers/ConvexClerkProvider";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    
 
+    <ConvexClerkProvider>
       <div>
         Hello
       </div>
-    
+    </ConvexClerkProvider>
+
   );
 }
